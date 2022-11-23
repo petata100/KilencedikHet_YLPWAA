0581014183534a2c3922d26a973ff9d9f14e4add
Péter Szabados <peter.szabados@stud.uni-corvinus.hu>
2022-11-23 16:40:49 +0100

    TizedikHet

---
+++ KilencedikHet_YLPWAA/wwwroot/geek.js
@@ -0,0 +1,21 @@
+﻿var viccek;
+
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    let jokes = document.getElementById("jokes");
+    console.log("Sikeres letöltés");
+    console.log(d);
+    viccek = d;
+
+    for (i = 0; i < viccek.length; i++) {
+        let joke = document.createElement("div");
+        joke.innerHTML = "<h1>" + viccek[i].question + "</h1>";
+        jokes.appendChild(joke);
+    }
+}
