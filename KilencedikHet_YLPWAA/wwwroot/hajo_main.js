6291ddee77278a7af97c9af2140ebd8bebe65fa5
ylpwaa <ylpwaa@SZA0030903>
2022-12-07 16:58:22 +0100

    hajós kieg. 12.07.

--- KilencedikHet_YLPWAA/wwwroot/hajo_main.js
+++ KilencedikHet_YLPWAA/wwwroot/hajo_main.js
@@ -1,6 +1,4 @@
-﻿fetch("hajo/kerdesek/4")
-    .then(response => response.json())
-    .then(data => showQ(data))
+﻿var id = 1;
 
 function showQ(q) {
     document.getElementById("kérdés").innerText = q.question1
@@ -9,3 +7,25 @@ function showQ(q) {
     document.getElementById("válasz3").innerText = q.answer3
     document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + q.image;
 }
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => showQ(data));
+}    
+
+function next() {
+    kérdésBetöltés(id + 1);
+}
+
+function back() {
+    kérdésBetöltés(id - 1);
+}
+
